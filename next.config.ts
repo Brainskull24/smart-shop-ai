9ed71b526fb5b9e4ae4515adf778210d6b096ed0
Brainskull <garg.nimit36@gmail.com>
2025-08-11 23:38:43 +0530

    fixed build issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.externals.push("chrome-aws-lambda", "puppeteer-core");
+    }
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      loader: "ignore-loader",
+      include: /node_modules\/chrome-aws-lambda/,
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
